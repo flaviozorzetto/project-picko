fbcf9b7803fe4e3a05a05b0946557cef89fc530a
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-07-06 15:31:21 -0300

    updated backward logic

--- src/components/Modals/InterviewModal/Competences/Competences.js
+++ src/components/Modals/InterviewModal/Competences/Competences.js
@@ -9,13 +9,14 @@ export default function Competences(props) {
 	const [searchInput, setSearchInput] = useState('');
 	const [stepManager, setStepManager] = useState({
 		mode: 'frontward',
+		closing: false,
 		step: 1,
 	});
 
-	const [closeManager, setCloseManager] = useState({
-		step: stepManager.step,
-		closing: false,
-	});
+	// const [closeManager, setCloseManager] = useState({
+	// 	step: stepManager.step,
+	// 	closing: false,
+	// });
 
 	const handleSearchInput = e => {
 		setSearchInput(e.target.value);
@@ -31,14 +32,14 @@ export default function Competences(props) {
 	};
 
 	const handleCloseClick = step => {
-		setCloseManager({ step, closing: true });
+		setStepManager({ mode: 'backward', step: step - 1, closing: true });
 		const turnOffSettings = () => {
 			if (step === 1) {
 				props.setCompetenceSettings(false);
 			}
 		};
 		delayedFunction(turnOffSettings, 500).then(() => {
-			setCloseManager({ step, closing: false });
+			setStepManager({ mode: 'backward', step: step - 1, closing: false });
 		});
 	};
 
@@ -47,7 +48,7 @@ export default function Competences(props) {
 			<div className="modal__competence__backdrop"></div>
 			<div
 				className={`modal__competence modal__competence__container${
-					closeManager.step === 1 && closeManager.closing
+					stepManager.step === 0
 						? ' modal__competence__container_mtr'
 						: stepManager.step === 1 && stepManager.mode === 'backward'
 						? ' modal__competence__container_ltm'
@@ -78,18 +79,17 @@ export default function Competences(props) {
 					theme="primary"
 					size="b"
 					onClick={() => {
-						setStepManager({ mode: 'frontward', step: 2 });
+						setStepManager({ ...stepManager, mode: 'frontward', step: 2 });
 					}}
 				/>
 			</div>
 			{stepManager.step === 2 && (
 				<div className="modal__competence__backdrop"></div>
 			)}
-			{(stepManager.step === 2 ||
-				(closeManager.step === 2 && closeManager.closing)) && (
+			{(stepManager.step === 2 || (stepManager.step === 1 && stepManager.closing === true)) && (
 				<div
 					className={`modal__competence modal__competence__container${
-						closeManager.step === 2 && closeManager.closing
+						stepManager.step === 1 && stepManager.closing
 							? ' modal__competence__container_mtr'
 							: ''
 					}`}
@@ -99,7 +99,6 @@ export default function Competences(props) {
 						theme="primary"
 						size="b"
 						onClick={() => {
-							setStepManager({ mode: 'backward', step: 1 });
 							handleCloseClick(stepManager.step);
 						}}
 					/>
