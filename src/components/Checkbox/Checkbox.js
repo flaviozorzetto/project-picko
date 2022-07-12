c737befa588f96ec70959f382a40fb4b73f7647e
juancrls <juan1112@outlook.com.br>
2022-07-12 14:55:42 -0300

    feat: added terms checkbox into the signup page

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react";
 import loadIcon from "../Elements/IconLoader/icon-loader";
+import { Link } from "react-router-dom";
+import { useValidation } from "../../contexts/ValidationContext.js";
+
 import "./Checkbox.scss";
 
 export default function Checkbox(props) {
-  const [input = {checked: props.checked ? props.checked : false}, setInput] = useState()
+  const { inputs, setInputs } = useValidation();
   
   useEffect(() => {
-    const checkbox = document.getElementById("checkbox");
+    const checkbox = document.getElementById(props.name);
     const checkboxContainerBackground = document.getElementsByClassName("checkbox-container-background")[0];
 
     checkbox.addEventListener("keydown", event => {
       if(event.code === "Space") {
-        console.log("activated with space", checkbox)
+        // console.log("activated with space", checkbox) // there's a bug in this useEffect that stacks the number of calls when the input is checked by the spacebar
 
-        if(input.checked) {
+        if(inputs[props.name].value) {
           checkboxContainerBackground.style.animation = "deactivatingCheckbox 0.15s ease-in-out forwards";
-        } else if (!input.checked) {
+        } else if (!inputs[props.name].value) {
           checkboxContainerBackground.style.animation = "activatingCheckbox 0.15s ease-in-out forwards";
         }
       }
@@ -27,21 +30,32 @@ export default function Checkbox(props) {
         checkboxContainerBackground.style.animation = "";
       }
     })
-  }, [input])
+  }, [inputs[props.name]])
 
   const handleChange = (event) => {
-    setInput({checked: event.target.checked})
+    setInputs((prev) => ({
+      ...prev,
+      [props.name]: {
+        ...prev[props.name],
+        message: "",
+      },
+    }));
+    // console.log("TESTE", inputs)
+
+    props.onChange(event)
   }
 
   return (
     <>
-      <div className="checkbox-container">
-        <input defaultChecked={input.checked} onChange={handleChange} type="checkbox" className="checkbox" id={props.name} disabled={props.disabled}/>
-        <i className="check-icon">{loadIcon("check", { width: 18, height: 18 })}</i>
-        <div className="checkbox-background"></div>
-        <label className="checkbox-label" htmlFor={props.name}></label>
-        <div className="checkbox-container-background"></div>
-      </div>
+      <label className="checkbox-label" htmlFor={props.name}>
+        <div className="checkbox-container">
+          <input defaultChecked={inputs[props.name] ? inputs[props.name].value : null} name={props.name} onChange={handleChange} type="checkbox" className="checkbox" id={props.name} disabled={props.disabled}/>
+          <i className="check-icon">{loadIcon("check", { width: 18, height: 18 })}</i>
+          <div className="checkbox-background"></div>
+          <div className="checkbox-container-background"></div>
+        </div>
+        {props.content}
+      </label>
     </>
   )
 }
